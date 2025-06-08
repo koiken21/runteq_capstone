9eae374c3dd4662e46d5da61a1589d839c27dd66
NKOIKE <koikeken@koikekennoMacBook-Air.local>
2025-06-08 20:14:28 +0900

    “タスク生成ページの有効化”

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,5 +2,5 @@ class Task < ApplicationRecord
   belongs_to :organization
   has_many :applications, dependent: :destroy
 
-  STATUS_OPTIONS = ["公開待ち", "募集中", "募集終了", "募集停止"].freeze
+  STATUS_OPTIONS = [ "公開待ち", "募集中", "募集終了", "募集停止" ].freeze
 end
