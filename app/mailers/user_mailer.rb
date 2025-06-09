a38d9d8d1425c66dded25020f27ea68870b2f3f0
koiken21 <koiken910@gmail.com>
2025-06-09 15:49:29 +0900

    Use URL helper for registration link

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,13 +2,7 @@ class UserMailer < ApplicationMailer
   def registration_email(user, initial_password)
     @user = user
     @initial_password = initial_password
-    @url = complete_registration_url(token: user.registration_token)
+    @url = complete_registration_users_url(token: user.registration_token)
     mail(to: @user.mail_address, subject: 'Complete your registration')
-  end
-
-  private
-
-  def complete_registration_url(token:)
-    "http://example.com/users/complete_registration?token=#{token}"
   end
 end
