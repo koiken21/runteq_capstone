50c5e48e43b534b6833a69c72b35a269223b87a3
koiken21 <koiken910@gmail.com>
2025-06-10 11:04:42 +0900

    Add GET route for logout

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,7 @@ Rails.application.routes.draw do
   get    'login',  to: 'sessions#new'
   post   'login',  to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
+  get    'logout', to: 'sessions#destroy'
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
   end
